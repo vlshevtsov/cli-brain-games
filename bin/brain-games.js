36069d9e8fd16dab27512f3b2c6152fd18fd3517
vlshevtsov <vladimir.shevtsov@aviasales.ru>
2023-12-18 19:00:02 +0400

    fix brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import askName from '../src/cli.js';
 
-const userName = askName(); 
+askName(); 
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const askName = () => {
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  return userName;
+  console.log('Hello, ' + userName);
 };
 
 export default askName;
