cbfb3c28e20b151e9f8f287066ba2951d44f7aa6
vlshevtsov <vladimir.shevtsov@aviasales.ru>
2023-12-18 18:36:55 +0400

    add brain-prime game

---
+++ src/games/prime.js
@@ -0,0 +1,13 @@
+import { run } from '../index.js';
+import { getRandomNumber, isPrime } from '../utils.js';
+
+const desc = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const getQA = () => {
+  const question = getRandomNumber(100);
+  let correctAnswer = isPrime(question) ? 'yes' : 'no';
+
+  return [question, correctAnswer];
+};
+
+export default () => run(desc, getQA);
--- src/utils.js
+++ src/utils.js
@@ -10,4 +10,15 @@ export const getEuclidGCD = (a, b) => {
 
 export const getProgression = (fn, start, count) => {
   return new Array(count).fill(0).map((elem, index) => fn(start + index));
-}
+};
+
+export const isPrime = (number) => {
+  if (number < 2) return false;
+  const stop = number / 2;
+  const iter = (counter) => {
+    if (counter > stop) return true;
+    if (number % counter === 0) return false;
+    return iter(counter + 1);
+  };
+  return iter(2);
+};
